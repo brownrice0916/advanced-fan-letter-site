a72d3c18df62318f22534938d15f2dba00102f6e
HwangHyeonmi <49122297+HwangHyeonmi@users.noreply.github.com>
2024-02-21 15:06:19 +0900

    Refactor user api 상수로 변경

--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -1,13 +1,14 @@
 import SignForm from "components/SignForm";
 import { useNavigate } from "react-router-dom";
 import authApi from "../../axios/authApi";
+import { REGISTER_ENDPOINT } from "constants/userConstants";
 
 const SignUp = () => {
   const navigate = useNavigate();
 
   const handleSignUp = async (id, pw, nickname) => {
     try {
-      const response = await authApi.post("/register", {
+      await authApi.post(REGISTER_ENDPOINT, {
         id: id,
         password: pw,
         nickname: nickname,
